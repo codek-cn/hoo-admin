58896ed583b03b2ce814c4b92fca7b14aa1a7b89
殷绪凯 <1922565649@qq.com>
2023-05-04 09:23:41 +0800

    模块重命名 暂提

--- hoo-modules/hoo-gen/src/main/java/com/ruoyi/gen/RuoYiGenApplication.java
+++ hoo-modules/hoo-gen/src/main/java/com/ruoyi/gen/RuoYiGenApplication.java
@@ -20,15 +20,5 @@ public class RuoYiGenApplication
     public static void main(String[] args)
     {
         SpringApplication.run(RuoYiGenApplication.class, args);
-        System.out.println("(♥◠‿◠)ﾉﾞ  代码生成模块启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
-                " .-------.       ____     __        \n" +
-                " |  _ _   \\      \\   \\   /  /    \n" +
-                " | ( ' )  |       \\  _. /  '       \n" +
-                " |(_ o _) /        _( )_ .'         \n" +
-                " | (_,_).' __  ___(_ o _)'          \n" +
-                " |  |\\ \\  |  ||   |(_,_)'         \n" +
-                " |  | \\ `'   /|   `-'  /           \n" +
-                " |  |  \\    /  \\      /           \n" +
-                " ''-'   `'-'    `-..-'              ");
     }
 }
